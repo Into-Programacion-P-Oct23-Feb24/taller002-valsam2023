c21e154ea559ec513f860e68aa36ece36ec9c420
valsam2023 <valentinasamaniego5@gmail.com>
2023-10-19 16:47:48 -0500

    segudno commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -14,7 +14,8 @@ public class Proyecto01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    System.out.println("Ejemplo de Java Manejado desde GitHub Versionado a traves de Git Creado por valsam2023");
+// TODO code application logic here
     }
     
 }
